ddf71e6e7f162fb90d49b0f8e4a62b609e17cd90
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-06-01 10:28:35 +0700

    add:Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import Text from "./components/Desc"
-import Button from "./components/Sosial"
+import Sosial from "./components/Button"
 
 function App() {
 
@@ -7,8 +6,12 @@ function App() {
     <div className="bg-black min-h-screen flex justify-center items-center">
       <div className="bg-[#1F1F1F] min-h-[550px] w-[400px] rounded-2xl flex justify-center flex-col items-center py-5">
         <img src="/Foto.jpg" alt="Foto" className="rounded-full h-[180px] w-[180px] my-2" />
-        <Text/>
-        <Button/>
+        <div className="text-center flex flex-col">
+            <h1 className="text-white">Putu Rivan Anggana Dharmawangsa</h1>
+            <p className="text-[#c5f82a] ">Jakarta, Indonesia</p>
+            <p className="text-gray-300 my-3 text-[14px]">"Informatics Students"</p>
+        </div>
+        <Sosial/>
       </div>
     </div>
   )
---
+++ src/components/Button.jsx
@@ -0,0 +1,48 @@
+import { XLogo } from "@phosphor-icons/react";
+import React from "react";
+
+const Sosial = () => {
+  const data = [
+    {
+      id: "1",
+      name: "Github",
+      link: "https://github.com/PutuRivan",
+      Logo: "/github-logo.png",
+    },
+    {
+      id: "2",
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/putu-rivan-4758a5285",
+      Logo: "/linkedin.png",
+    },
+    {
+      id: "3",
+      name: "X",
+      link: "https://twitter.com/koreeeet",
+      Logo: "twitter.png",
+    },
+    {
+      id: "4",
+      name: "Instagram",
+      link: "https://www.instagram.com/rivanputu_",
+      Logo: "instagram.png",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-4">
+      {data.map((item) => {
+        return (
+          <a href={item.link} key={item.id} target="_blank">
+            <div className="w-[300px] bg-[#333] p-2 rounded-xl grid grid-cols-3 hover:scale-105 hover:text-white">
+              <img src={item.Logo} alt=""/>
+              <p className="text-center">{item.name}</p>
+            </div>
+          </a>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Sosial;
